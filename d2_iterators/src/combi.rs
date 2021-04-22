aace73a24964b0aaca57005618a3fc67aa315ca9
CatLava <evan.schoening@gmail.com>
2021-04-22 10:55:20 -0700

    testing iterators and test cases
    what is the difference between lib and main cargo packages

--- d2_iterators/src/combi.rs
+++ d2_iterators/src/combi.rs
@@ -1,3 +1,5 @@
+
+use itertools::Itertools;
 // Iterator that skips every other element
 
 pub struct SkipIterator<I: Iterator> {
@@ -33,5 +35,24 @@ mod test_skip{
     fn test_skip_half(){
         let v : i32 = (0..10).skip_half().sum();
         assert_eq!(v, 1+3+5+7+9);
+    }
+
+    #[test]
+    fn test_step_by(){
+        let v: i32 = (0..10).step_by(3).sum();
+        assert_eq!(v, 0+3+6+9);
+    }
+
+    #[test]
+    fn test_interleave(){
+        let v:Vec<i32> = (0..4).interleave((11..15).rev()).collect();
+        assert_eq!(v,vec![0,14,1,13,2,12,3,11]);
+    }
+
+    #[test]
+    fn test_intersperse(){
+        let s = "hello world etc";
+        let v: Vec<&str> = s.split(" ").intersperse(",").collect();
+        assert_eq!(v, vec!["hello",",","world",",","etc"])
     }
 }
